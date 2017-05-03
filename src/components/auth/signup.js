b700cc284dcc932a74bdbde13e7f6f912a64e744
Jason Vance <jbvance@gmail.com>
2017-05-03 10:57:57 -0500

    Finish signup features

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -3,11 +3,26 @@ import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 
 class Signup extends Component {
+  handleFormSubmit(formProps){
+    //Call action creator to sign up the user
+    this.props.signupUser(formProps);
+  }
+
+  renderAlert(){
+    if (this.props.errorMessage){
+      return (
+        <div className="alert alert-danger">
+          <strong>Oops! </strong>{this.props.errorMessage}
+        </div>
+      )
+    }
+  }
+
   render() {
     //TODO refactor input fields into a function to prevent repeating same code three times
     const  { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
         <fieldset className="form-group">
         <label>Email:</label>
         <input className="form-control" {...email } />
@@ -25,6 +40,7 @@ class Signup extends Component {
         <input type="password" className="form-control" {...passwordConfirm } />
         {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
         </fieldset>
+        { this.renderAlert() }
         <button action="submit" className="btn btn-primary">Sign Up </button>
       </form>
     );
@@ -50,11 +66,16 @@ function validate(formProps) {
     errors.password = 'Passwords must match';
   }
 
+ //console.log("ERRORS", errors);
   return errors;
+}
+
+function mapStateToProps(state){
+  return { errorMessage: state.auth.error}
 }
 
 export default reduxForm({
   form: 'signup',
   fields: ['email', 'password', 'passwordConfirm'],
   validate: validate
-})(Signup);
+}, mapStateToProps, actions)(Signup);
