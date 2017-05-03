f0e3b24f30110ae405088210a159a7447f1cb06d
Jason Vance <jbvance@gmail.com>
2017-05-03 11:21:22 -0500

    Add index route component

---
+++ src/components/welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+class Welcome extends Component {
+  render(){
+    return(
+      <div>Welcome to the Homepage!</div>
+    );
+  }
+}
+
+export default Welcome;
